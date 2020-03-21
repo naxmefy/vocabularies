3fa0064077c0af96cbcf685ad16fda67e9de5759
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-03-21 12:09:45 +0100

    resolve permitting
    actual warning of
    https://github.com/zeit/next.js/issues/10439#issuecomment-583214126

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,7 +1,12 @@
 import authenticated from '../../middlewares/authenticated'
-import handler from '../../middlewares'
+import connect from 'next-connect'
+import middlewares from '../../middlewares'
 
+const handler = connect()
+
+handler.use(middlewares)
 handler.use(authenticated)
+
 handler.get((req, res) => {
   res.json({
     rofl: 'mao',
--- pages/api/sign-in.js
+++ pages/api/sign-in.js
@@ -1,14 +1,18 @@
-import handler from '../../middlewares'
+import connect from 'next-connect'
+import middlewares from '../../middlewares'
 import permitParams from '../../lib/permit-params'
 
+const handler = connect()
+handler.use(middlewares)
 handler.use(
   permitParams({
-    'body.email': 'required email',
+    'body.email': 'required:email',
     'body.password': 'required',
   }),
 )
 handler.post((req, res) => {
   // const { email, password } = req.body
+  // TODO
   res.status(204).end()
 })
 
